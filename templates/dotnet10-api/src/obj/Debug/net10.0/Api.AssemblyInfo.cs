a61f62722766156e19a0721e907d01aa1fd3ed82
IngKendrys <kendrys333@gmail.com>
2026-03-17 21:20:42 +0000

    lab02: Solución codespaces y devcontainer reproducible

--- templates/dotnet10-api/src/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ templates/dotnet10-api/src/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af8c2e0da2900a222bce8388691f62e0fae57b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
